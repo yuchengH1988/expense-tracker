702fad14757cb21b3d2481d7d5653e57098b577e
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-02-16 21:21:03 +0800

    feat: project innit & add mongoose

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})

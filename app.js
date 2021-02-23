0058e9a3a61008844041b5a7893cddd8028b94c4
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-02-23 11:47:11 +0800

    feat: add sort function, and index page selectedcategory

--- app.js
+++ app.js
@@ -29,21 +29,27 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
+  const seletedCategory = req.query.category || ''
+  const filter = {}
+  if (seletedCategory) {
+    filter.category = seletedCategory
+  }
   let totalAmount = 0
   const categories = []
   Category.find()
     .lean()
     .then((items) => {
       categories.push(...items)
-      Record.find()
+      Record.find(filter)
         .lean()
+        .sort({ date: 'asc' })
         .then(records => {
           records.forEach(record => {
             totalAmount += Number(record.amount)
             record.date = record.date.toString().slice(4, 15)
             record.iconHTML = generateIconHTML(record.category)
           })
-          res.render('index', { records, categories, totalAmount })
+          res.render('index', { records, categories, totalAmount, seletedCategory })
         })
         .catch(error => console.error(error))
     })

5e5091b87edd5379557998ffd67c9e456c5b3fc3
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-25 16:48:38 +0800

    feat: add more record data

--- models/seeds/records.js
+++ models/seeds/records.js
@@ -7,9 +7,23 @@ const records = [
     merchant: '7-11'
   },
   {
+    name: '香蕉',
+    category: '餐飲食品',
+    date: '2021-02-05',
+    amount: 100,
+    merchant: '7-11'
+  },
+  {
     name: '計程車費',
     category: '交通出行',
     date: '2021-01-06',
+    amount: 350,
+    merchant: 'Taxi'
+  },
+  {
+    name: '計程車費',
+    category: '交通出行',
+    date: '2021-03-06',
     amount: 350,
     merchant: 'Taxi'
   },
@@ -21,6 +35,13 @@ const records = [
     merchant: '錢櫃'
   },
   {
+    name: '卡拉OK',
+    category: '休閒娛樂',
+    date: '2021-01-07',
+    amount: 1000,
+    merchant: '錢櫃'
+  },
+  {
     name: '罰單',
     category: '其他',
     date: '2021-02-08',
@@ -28,9 +49,16 @@ const records = [
     merchant: '政府'
   },
   {
+    name: '罰單',
+    category: '其他',
+    date: '2021-03-08',
+    amount: 900,
+    merchant: '政府'
+  },
+  {
     name: 'iphone',
     category: '家居物業',
-    date: '2020-12-09',
+    date: '2021-03-09',
     amount: 37000,
     merchant: 'iphone'
   }
